7c0243b515710e54ea9997da14bfde300fd5abfe
vaibhavmdesai <vaibhavdesai90@gmail.com>
2024-12-26 14:02:11 +0530

    changed config file path

--- dags/ingestion/businesscode/constants.py
+++ dags/ingestion/businesscode/constants.py
@@ -4,7 +4,7 @@ import csv
 postgres_conn_id = 'telecom_postgres' 
 aws_conn_id='aws_default'
 s3_bucket = 'oabronze'
-config_path = '/home/vd/data-engineering/projects/Telecom/configs/config.csv'
+config_path = '/home/vd/data-engineering/projects/Telecom/telecom-env/code/configs/config.csv'
 archive_date = datetime.today().strftime('%Y/%m/%d')
 archive_suffix = datetime.today().strftime('%Y%m%d%H%M%S')
 
